982e017d6b287b94225ff148ecc8f1471a52faca
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-10-14 18:04:18 +0900

    section1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -8,7 +8,7 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(50 + 22, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI);// BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
