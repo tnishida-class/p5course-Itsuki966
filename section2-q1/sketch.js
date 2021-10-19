f94ead62e91ea645d7ee10d7377e13710f789e75
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-10-19 12:19:52 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100);
+  background(255);
+
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let r1 = 100 - i*10
+    if(i <= 4){
+      stroke(255,0,0);
+      ellipse(50,50,r1);
+    }
+    else{
+      stroke(0,0,255);
+      ellipse(50,50,r1);
+    }// BLANK[1]
   }
 }
