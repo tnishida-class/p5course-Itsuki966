94eba2319a0653e40996a43b9fd772490c3bbd07
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-10-23 16:28:44 +0900

    section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -10,22 +10,37 @@ function setup() {
   stroke(255);
   strokeWeight(3);
 
+  const width = 400;
+  const height = 400;
   const cx = width / 2; // 中心は (cx, cy)
   const cy = height / 2;
   const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawArcs(cream,black,maxR * 0.75);
+  drawArcs(green,red,maxR * 0.5);
+  drawArcs(cream,black,maxR * 0.45);
+  drawCircle(green,maxR * 0.1);// BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
   drawCircle(red, maxR * 0.05);
 }
 
 function drawCircle(c, r){
+  const width = 400;
+  const height = 400;
+  const cx = width / 2;
+  const cy = height / 2;
+  const maxR = min(width, height);
   fill(c);
   ellipse(cx, cy, r, r);
 }
 
 function drawArcs(c1, c2, r) {
+  const width = 400;
+  const height = 400;
+  const cx = width / 2;
+  const cy = height / 2;
+  const maxR = min(width, height);
   for (let i = 0; i < 20; i++) {
     let start = TWO_PI / 20 * i;
     let stop = TWO_PI / 20 * (i + 1);
