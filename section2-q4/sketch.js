7a9fb5a311c17559a5bd68853eb2cd002b953fc3
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-10-23 16:54:03 +0900

    section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,19 +2,30 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i % 2 == 0){
+      fill(blue);
+    }
+    else{
+      fill(255);
+    }
+// BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
   }
 
   fill(blue);
   let size = d * 5;
   rect(0, 0, size, size);
+  strokeWeight(d);
+  strokeCap(SQUARE);
+  stroke(255);
+  line(size/2,0,size/2,size);
+  line(0,size/2,size,size/2);
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
