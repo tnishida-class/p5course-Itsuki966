c93f36c9e58fac5946bd861322f88b5c8da4a0ed
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-11-02 02:20:06 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,13 +4,19 @@ let cycle;
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
+  count = 85;
+  cycle = 60;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + 1) % cycle
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  let size = count;
+  fill(255,105,180);
+  noStroke();
+  arc(width / 3 + 7,height / 2, size + 48,size + 48,PI,2 * PI,OPEN);
+  arc(2 * width / 3 - 8,height / 2, size + 48,size + 48,PI,2 * PI,OPEN);
+  arc(width / 2,height / 2, size + 100,size + 105,0,PI,OPEN);
+  //ellipse(width / 2, height / 2,size + 95);
 }
