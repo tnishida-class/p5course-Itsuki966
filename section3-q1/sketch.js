6a1b9924afb3b6b7e2868f7d1d2a6ab329f401ee
Itsuki966 <92083270+Itsuki966@users.noreply.github.com>
2021-11-02 02:46:44 +0900

    section3-q1-2
    上矢印と下矢印を押すとそれぞれ鼓動のスピードが変わるようにしました。

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,9 +3,9 @@ let count;
 let cycle;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(200, 300);
   count = 85;
-  cycle = 60;
+  cycle = 30;
 }
 
 function draw(){
@@ -13,10 +13,12 @@ function draw(){
   count = (count + 1) % cycle
   // BLANK[1]
   let size = count;
+  if(keyIsDown(UP_ARROW)){ cycle = 10;}
+  if(keyIsDown(DOWN_ARROW)){ cycle = 61;}
   fill(255,105,180);
   noStroke();
-  arc(width / 3 + 7,height / 2, size + 48,size + 48,PI,2 * PI,OPEN);
-  arc(2 * width / 3 - 8,height / 2, size + 48,size + 48,PI,2 * PI,OPEN);
-  arc(width / 2,height / 2, size + 100,size + 105,0,PI,OPEN);
-  //ellipse(width / 2, height / 2,size + 95);
+  arc(width / 3 + 7,height / 3, size + 48,size + 48,PI,2 * PI,OPEN);
+  arc(2 * width / 3 - 8,height / 3, size + 48,size + 48,PI,2 * PI,OPEN);
+  arc(width / 2,height / 3, size + 100,size + 200,0,PI,OPEN);
+
 }
